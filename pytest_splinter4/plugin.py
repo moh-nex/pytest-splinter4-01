07e1b110131a38890bb0b240f76a550423b2ef3d
Mohammad Islam <mohammad.islam@nexonia.com>
2022-09-28 15:26:45 -0400

    implement service instead executable_path

--- pytest_splinter4/plugin.py
+++ pytest_splinter4/plugin.py
@@ -17,7 +17,7 @@ from _pytest import junitxml
 import pytest  # pragma: no cover
 
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support import wait
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -275,7 +275,7 @@ def _splinter_driver_default_kwargs(splinter_logs_dir, splinter_remote_name):
     driver_kwargs = {
         'chrome': {
             # 'executable_path': get_executable_path(cwd, 'chromedriver'),
-            'service': Service(ChromeDriverManager().install()),
+            'service': ChromeService(ChromeDriverManager().install()),
             'service_args': [
                 '--verbose',
                 f"--log-path={splinter_logs_dir}/chromedriver.log",
