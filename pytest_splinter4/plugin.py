a82ce21b302a81736063c0e4ef903ea9f48c09e1
moh-nex <mohammad.islam@emburse.com>
2024-02-29 18:29:17 -0500

    changing the take_screenshot function

--- pytest_splinter4/plugin.py
+++ pytest_splinter4/plugin.py
@@ -431,11 +431,12 @@ def _take_screenshot(
 
     screenshot_file_name = f"{name_0}-{fixture_name}".replace(os.path.sep, "-")
 
-    slaveoutput = getattr(request.config, "workerinput", None)
-    if not slaveoutput:
-        os.makedirs(screenshot_dir, exist_ok=True)
-    else:
-        screenshot_dir = session_tmpdir.ensure("screenshots", dir=True).strpath
+    slaveoutput = getattr(request.config, "workeroutput", None)
+    # if not slaveoutput:
+    #     os.makedirs(screenshot_dir, exist_ok=True)
+    # else:
+    #     screenshot_dir = session_tmpdir.ensure("screenshots", dir=True).strpath
+    os.makedirs(screenshot_dir, exist_ok=True)
 
     screenshot_path = os.path.join(screenshot_dir, screenshot_file_name)
 
